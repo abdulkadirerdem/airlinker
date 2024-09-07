a13cbcb5a963ff40a677cf9bdbd64446463ab731
Cengizhan KOSE <cengizhan.kose@monsternotebook.com>
2024-09-08 02:39:47 +0300

    add responses submit raffle

--- src/api/quiz/submitQuiz.ts
+++ src/api/quiz/submitQuiz.ts
@@ -1,5 +1,5 @@
 import { endpoints } from 'src/utils/axios';
 import { postData } from 'src/utils/request';
 
-export const submitForm = async ({ id, data }: { id: string; data: any }): Promise<any[]> =>
+export const submitQuiz = async ({ id, data }: { id: string; data: any }): Promise<any[]> =>
   postData<any[]>(endpoints.quizzes.submitQuiz(id), data);
--- src/components/data-grid.tsx
+++ src/components/data-grid.tsx
@@ -48,9 +48,9 @@ export default function DataGridTable({ data, error, selectedWorkspaceId }: Prop
             variant="text"
             // sx={{ cursor: 'pointer' }}
             onClick={() => {
-              router.push(`/form/${selectedWorkspaceId}/${params.id}`);
+              router.push(`/${params.row.type}/${selectedWorkspaceId}/${params.id}`);
             }}
-          >{`localhost:8083/form/${selectedWorkspaceId}/${params.id}`}</Button>
+          >{`localhost:8083/${params.row.type}/${selectedWorkspaceId}/${params.id}`}</Button>
         ),
       },
       {
