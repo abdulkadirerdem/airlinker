76a7ef3943495b5a6cd0334974994f34385a0b29
abdulkadirerdem <akadir_1256@hotmail.com>
2024-09-08 05:22:38 +0300

    widget design done

--- src/components/widget-panel.tsx
+++ src/components/widget-panel.tsx
@@ -1,34 +1,45 @@
 import { OverridableStringUnion } from '@mui/types';
 import { ButtonPropsColorOverrides } from '@mui/material/Button';
-import { Box, Stack, Button, Divider, Typography } from '@mui/material';
+import { Box, Stack, Button, Divider, Typography, Grid } from '@mui/material';
 import Iconify from './iconify';
 
 const widgets: Array<{
   type: string;
   label: string;
   defaultOptions?: [] | [string] | any;
-  icon: string,
-  color: OverridableStringUnion<
-    'primary' | 'secondary' | 'info' | 'error' | 'warning' | 'success' | 'inherit',
-    ButtonPropsColorOverrides
-  >;
+  icon: string;
+  category: 'Primary' | 'Secondary' | 'Other';
 }> = [
     {
       type: 'radio',
       label: 'Radio Button',
       defaultOptions: ['Option 1', 'Option 2'],
-      color: 'info',
-      icon: "ci:radio-fill"
+      icon: "ci:radio-fill",
+      category: 'Primary'
     },
     {
       type: 'multiple-choice',
       label: 'Multiple Choice',
       defaultOptions: ['Option 1', 'Option 2'],
-      color: 'error',
-      icon: "healthicons:i-exam-multiple-choice-outline"
+      icon: "healthicons:i-exam-multiple-choice-outline",
+      category: 'Primary'
     },
-    { type: 'text', label: 'Text Field', color: 'warning', icon: "solar:text-bold" },
-    { type: 'connect-wallet', label: 'Connect Wallet', color: 'success', icon: "mingcute:solana-sol-line" },
+    { type: 'text', label: 'Text Field', icon: "solar:text-bold", category: 'Secondary' },
+    { type: 'connect-wallet', label: 'Connect Wallet', icon: "mingcute:solana-sol-line", category: 'Primary' },
+    // Example data for other categories
+    { type: 'checkbox', label: 'Checkbox', icon: "ic:outline-check-box", category: 'Secondary' },
+    { type: 'phone', label: 'Phone', icon: "ph:phone", category: 'Secondary' },
+    { type: 'email', label: 'Email', icon: "mage:email", category: 'Secondary' },
+    { type: 'location', label: 'Location', icon: "mingcute:location-line", category: 'Secondary' },
+    { type: 'yes-no', label: 'Yes/No', icon: "humbleicons:switch-off", category: 'Secondary' },
+    { type: 'file-upload', label: 'File Uploads', icon: "fluent:attach-32-regular", category: 'Secondary' },
+    { type: 'slider', label: 'Slider', icon: "fluent-mdl2:slider", category: 'Secondary' },
+    { type: 'datepicker', label: 'Date Picker', icon: "ic:outline-event", category: 'Secondary' },
+    { type: 'dropdown', label: 'Dropdown', icon: "mdi:form-dropdown", category: 'Other' },
+    { type: 'date-time', label: 'Date Time', icon: "solar:calendar-outline", category: 'Other' },
+    { type: 'open-question', label: 'Open Question', icon: "iconoir:align-right-box", category: 'Other' },
+    // { type: 'section-break', label: 'Section Break', icon: "mdi:form-dropdown", category: 'Other' },
+
   ];
 
 export default function WidgetPanel({
@@ -36,28 +47,47 @@ export default function WidgetPanel({
 }: {
   onSelect: (type: string, options?: string[]) => void;
 }) {
+  const renderWidgets = (category: 'Primary' | 'Secondary' | 'Other') => (
+    <Stack spacing={1} mb={2}>
+      <Typography variant="subtitle1" fontWeight={600} color="lightgrey" >
+        {category}
+      </Typography>
+      <Grid container spacing={1}>
+        {widgets
+          .filter(widget => widget.category === category)
+          .map(widget => (
+            <Grid item xs={6} sm={widget.category === "Primary" ? 4 : 4} key={widget.type}>
+              <Button
+                sx={{
+                  background: "#F6F6FB",
+                  borderRadius: 0.3,
+                  width: '100%',
+                  height: 100,
+                  p: 2,
+                  alignSelf: "baseline",
+                  ":hover": {
+                    color: "white"
+                  }
+                }}
+                onClick={() => onSelect(widget.type, widget.defaultOptions)}
+                variant="contained"
+              >
+                <Stack alignItems={"center"} gap={1}>
+                  <Iconify width={36} icon={widget.icon} color={"lightgrey"} />
+                  <Typography variant='caption' color='textSecondary' sx={{ ":hover": { color: "inherit" } }} fontWeight={700}>{widget.label}</Typography>
+                </Stack>
+              </Button>
+            </Grid>
+          ))}
+      </Grid>
+    </Stack >
+  );
+
   return (
     <Box p={2} width={"100%"}>
-      <Typography variant="h6" fontWeight={500} mb={0.5}>
-        Widgets
-      </Typography>
-      <Divider sx={{ mb: 2.25, borderColor: "lightgray" }} />
-      <Stack spacing={1} flexWrap="wrap" flexDirection="row" justifyContent={"center"} width={"100%"}>
-        {widgets.map((widget) => (
-          <Button
-            sx={{ background: "#F6F6FB", borderRadius: 0.3, width: "30%", p: 5, height: 100, alignSelf: "baseline" }}
-            key={widget.type}
-            onClick={() => onSelect(widget.type, widget.defaultOptions)}
-            variant="contained"
-
-          >
-            <Stack alignItems={"center"} gap={1}>
-              <Iconify width={36} icon={widget.icon} color={"lightgrey"} />
-              <Typography variant='caption' color='textPrimary' fontWeight={700} >{widget.label}</Typography>
-            </Stack>
-          </Button>
-        ))}
-      </Stack>
-    </Box >
+      {renderWidgets('Primary')}
+      {renderWidgets('Secondary')}
+      {renderWidgets('Other')}
+    </Box>
   );
 }
